5598b4dff3ae926ab2c06dd7c9853699aed7d145
Drakon58 <widowmaker58@live.ca>
2016-04-25 19:16:32 -0400

    Added in text alignment parameters for text monitor
    Additional assets have also been added to serve as a backdrop for the chat input

--- modules/custom_components/aFrameChatBoxInput.js
+++ modules/custom_components/aFrameChatBoxInput.js
@@ -11,7 +11,8 @@ AFRAME.registerComponent('chatbox', {
         heightSegments: {default: 1},
         textInput: {default: 'default1234'},
         textAlignment: {default: 'center'},
-        fontSize: {default: '40'}
+        fontSize: {default: '40'},
+        opacity: {default: 0.9}
     },
     init: function(){
         this.chatBox = document.createElement('a-entity');
@@ -19,10 +20,18 @@ AFRAME.registerComponent('chatbox', {
         this.chatBox.setAttribute("visible","false");
         this.chatBox.setAttribute("textmonitor","textInput:" + this.data.textInput +
             "; width: " + this.data.width +  "; height: " + this.data.height +
-            "; textAlignment: " + this.data.textAlignment + "; fontSize: " + this.data.fontSize);
+            "; textAlignment: " + this.data.textAlignment + "; fontSize: " + this.data.fontSize + "; opacity: " + this.data.opacity);
         this.chatBox.setAttribute("rotation" , "0 -30 0");
         this.chatBox.setAttribute("position" , (this.data.width/2 + 0.7) + " " + -(this.data.height * 1.2) + " -0.8");
         document.getElementById('actualCam').appendChild(this.chatBox);
+
+        this.chatBG = document.createElement('a-entity');
+        this.chatBG.setAttribute("geometry","primitive: plane; width: " + this.data.width + "; height: " + this.data.height);
+        this.chatBG.setAttribute("material","shader: standard; side: double; src:#ui-chat-bg; opacity: " + this.data.opacity/2);
+        this.chatBG.setAttribute("position" , "0 0 -0.001");
+        this.chatBG.setAttribute("scale" , "1.1 1.1 0");
+        this.chatBG.setAttribute("rotation" , "0 180 0");
+        document.getElementById('chatBox').appendChild(this.chatBG);
     }
 });
 
--- modules/custom_components/aFrameTextMonitor.js
+++ modules/custom_components/aFrameTextMonitor.js
@@ -5,6 +5,53 @@
 //Text monitor instance array
 
 var instanceNum=-1;
+/*
+Valid positions for text origin alignment are:
+topleft    topcenter    topright
+centerleft    center    centerright
+bottomleft    bottomcenter    bottomright
+ */
+function TextOriginSettings(){
+
+    var textXOffset = 0;
+    var textYOffset = 0;
+}
+
+TextOriginSettings.prototype.calcOriginPoint = function(context, textStartPos, text){
+    if(textStartPos == 'topleft'){
+        this.textXOffset = 0;
+        this.textYOffset = context.canvas.height - parseInt(context.font);
+    }else  if(textStartPos == 'topcenter'){
+        this.textXOffset = context.canvas.width/2;
+        this.textYOffset = context.canvas.height - parseInt(context.font);
+    }else  if(textStartPos == 'topright') {
+        this.textXOffset = context.canvas.width - context.measureText(text).width;
+        this.textYOffset = context.canvas.height - parseInt(context.font);
+    }else  if(textStartPos == 'centerleft'){
+        this.textXOffset = 0;
+        this.textYOffset = context.canvas.height/2;
+    }else  if(textStartPos == 'center'){
+        this.textXOffset = context.canvas.width/2;
+        this.textYOffset = context.canvas.height/2;
+    }else if(textStartPos == 'centerright'){
+        this.textXOffset = context.canvas.width - context.measureText(text).width;
+        this.textYOffset = context.canvas.height/2;
+    }else if(textStartPos == 'bottomleft') {
+        this.textXOffset = 0;
+        this.textYOffset = parseInt(context.font)/2;
+    }else if(textStartPos == 'bottomcenter') {
+        this.textXOffset = context.canvas.width/2;
+        this.textYOffset = parseInt(context.font)/2;
+    }else if(textStartPos == 'bottomright') {
+        this.textXOffset = context.canvas.width - context.measureText(text).width;
+        this.textYOffset = parseInt(context.font)/2;
+    }
+    else{
+        console.log(textStartPos + " is not a valid origin alignment in the canvas. Using middleleft as default.")
+        this.textXOffset = 0;
+        this.textYOffset = context.canvas.height/2;
+    }
+};
 
 AFRAME.registerComponent('textmonitor', {
     dependencies: ['material'],
@@ -16,7 +63,11 @@ AFRAME.registerComponent('textmonitor', {
         heightSegments: {default: 1},
         textInput: {default: 'default1234'},
         textAlignment: {default: 'center'},
-        fontSize: {default: 40}
+        fontSize: {default: 40},
+        textStartPos: {default: 'centerleft'},
+        textXOffset: {default:0},
+        textYOffset: {default:0},
+        opacity: {default:1.0}
     },
     init: function(){
         instanceNum++;
@@ -31,25 +82,36 @@ AFRAME.registerComponent('textmonitor', {
         this.fontSize = this.data.fontSize;
         this.ctx.textAlign = this.textAlignment;
         this.ctx.font = this.fontSize + 'pt Arial';
+        this.textStartPos = this.data.textStartPos;
+        //offsets and origins
+        this.textXOffset = this.data.textXOffset;
+        this.textYOffset = this.data.textYOffset;
+        this.textOriginSettings = new TextOriginSettings();
+
+
         this.texture = new THREE.Texture(this.canvas);
+
         console.log("Number of textMonitor instances: " + instanceNum);
         console.log("Font size for this monitor: " + this.fontSize);
+        console.log("Alignment is : " + this.textAlignment);
     },
     update: function(){
 
         this.texture = new THREE.Texture(this.canvas);
-        this.material = new THREE.MeshBasicMaterial({
+        this.material = new THREE.MeshPhongMaterial({
             color: this.data.color,
             side: THREE.DoubleSide,
             map: this.texture,
-            opacity: "0.8",
+            opacity: this.data.opacity,
             transparent:true
         });
 
         var geometry = new THREE.PlaneGeometry(this.data.width, this.data.height,
             this.data.widthSegments,this.data.heightSegments);
         this.plane = new THREE.Mesh(geometry, this.material);
+        this.plane.name = "textMonitorCanvas" + instanceNum;
         this.el.object3D.add(this.plane);
+
     },
     tick: function(){
         if(this.data.textInput == 'default1234'){
@@ -65,13 +127,14 @@ AFRAME.registerComponent('textmonitor', {
             }
         }
 
-        this.ctx.fillStyle = "rgba(0,0,0,1)";
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.textOriginSettings.calcOriginPoint(this.ctx, this.textStartPos, this.textSource);
+
         this.ctx.fillStyle = "rgba(255,255,255,1)";
-        this.ctx.fillRect(10, 10, this.canvas.width - 20, this.canvas.height - 20);
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.fillStyle = "rgba(0,0,0,1)";
         this.ctx.textBaseline = "middle";
-        this.ctx.fillText(this.textSource, 25, this.canvas.height / 2);
+        this.ctx.fillText(this.textSource, this.textXOffset + this.textOriginSettings.textXOffset,
+            this.textYOffset + this.textOriginSettings.textYOffset);
 
         this.plane.material.map.image = this.canvas;
         this.plane.material.map.needsUpdate = true;
